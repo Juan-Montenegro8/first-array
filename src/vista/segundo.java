a9f89b7c26e953ff2b8e1b3ab68fe14451efa53c
Juan <jj.monte2002@gmail.com>
2024-11-06 20:10:55 -0500

    Third commit

--- src/vista/segundo.java
+++ src/vista/segundo.java
@@ -3,18 +3,20 @@ import java.util.*;
 public class segundo {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner (System.in);
-        int invertido[]=new int[15];
+        try ( Scanner t=new Scanner (System.in)){
+            int invertido[]=new int[15];
         
-        System.out.println("ingrese numeros");
-        for (int i = 0; i < invertido.length; i++) {
-            invertido[i]=t.nextInt();
+            System.out.println("ingrese numeros");
+            for (int i = 0; i < invertido.length; i++) {
+                invertido[i]=t.nextInt();
+            }
+            System.out.println("******************la impresion**********************");
+            for (int i = 0; i < invertido.length; i++) {
+                System.out.println(""+invertido[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-        System.out.println("******************la impresion**********************");
-        for (int i = 0; i < invertido.length; i++) {
-            System.out.println(""+invertido[i]);
-        }
-        
     }
     
 }
