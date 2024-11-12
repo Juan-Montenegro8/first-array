14bdbf2dd355f6e6347e19045384b4b39b6c4e2b
Juan <jj.monte2002@gmail.com>
2024-11-11 23:37:04 -0500

    fifth commit

--- src/vista/quinta.java
+++ src/vista/quinta.java
@@ -6,51 +6,50 @@ public class quinta {
 
     public static void main(String[] args) {
         
-        Scanner t=new Scanner (System.in);
         int num[]=new int[3];
-        Random ale=new Random();
-        int prm=0;
+        Random aleatorio1=new Random();
+        int promedio1=0;
         System.out.println("****************************VECTOR 1*********************************");
         for (int i = 0; i < num.length; i++) {
-            num[i]=ale.nextInt(11);
+            num[i]=aleatorio1.nextInt(11);
             System.out.println("numero"+num[i]);
-            prm=num[0]+num[1]+num[2]/3;
+            promedio1=num[0]+num[1]+num[2]/3;
         }
         
         int num1[]=new int[3];
         Random ale1=new Random();
-        int prm1=0;
+        int promedio2=0;
         System.out.println("****************************VECTOR 2*********************************");
         for (int i = 0; i < num1.length; i++) {
             num1[i]=ale1.nextInt(11);
             System.out.println("numero"+num1[i]);
-            prm1=num[0]+num[1]+num[2]/3;
+            promedio2=num[0]+num[1]+num[2]/3;
         }
         
         int num2[]=new int[3];
         Random ale2=new Random();
-        int prm2=0;
+        int promedio3=0;
         System.out.println("****************************VECTOR 3*********************************");
         for (int i = 0; i < num2.length; i++) {
             num2[i]=ale2.nextInt(11);
             System.out.println("numero"+num2[i]);
-            prm2=num[0]+num[1]+num[2]/3;
+            promedio3=num[0]+num[1]+num[2]/3;
         }
         
-        if(prm<=prm1){
+        if(promedio1<=promedio2){
             System.out.println("el promedio del vector2 es mayor o igual al vector 1"
-                    + "con un promedio de "+prm1);
+                    + "con un promedio de "+promedio2);
         }else{
             System.out.println("el promedio del vector2 es menor al vector 1"
-                    + "con un promedio de"+prm1);
+                    + "con un promedio de"+promedio2);
         }
         
-        if(prm<=prm2){
+        if(promedio1<=promedio3){
            System.out.println("el promedio del vector3 es mayor o igual al vector 1"
-                   + "con un promedio de"+prm2); 
+                   + "con un promedio de"+promedio3); 
         }else{
             System.out.println("el promedio del vector3 menor al vector 1"
-                    + "con un promedio de"+prm2);
+                    + "con un promedio de"+promedio3);
         }
     }
     
