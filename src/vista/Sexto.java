c3f0a10d4966b2d0a152c647fb34e93c678d1816
Juan <jj.monte2002@gmail.com>
2024-11-12 20:27:56 -0500

    Seventh commit

--- src/vista/Sexto.java
+++ src/vista/Sexto.java
@@ -8,13 +8,32 @@ public class Sexto {
         int numeros[]= new int[10];
         Random aleatorio = new Random();
         int posicion=0;
+        int contenedor=0;
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i]=aleatorio.nextInt(11);
+            int numero;
+            boolean repetido;
+
+            do { 
+                repetido= false;
+                numero=aleatorio.nextInt(11);
+                for (int j = 0; j < i; j++){
+                    if (numeros[j]==numero) {
+                        repetido= true;
+                        break;
+                    }
+                }
+                    
+            }while (repetido);
             
-            System.out.println("el numero es "+numeros[i]+" y la posicion "+i);
-            
+            numeros[i]=numero;
+            System.out.println("El numero es "+numeros[i]+" y la posicion "+i);
+
+            if (contenedor<numeros[i]) {
+                contenedor=numeros[i];
+                posicion=i;
+            }
         }
-        System.out.println("la posicion del mayor es "+posicion);
+        System.out.println("La posicion del mayor es "+posicion+" y el numero es "+contenedor);
     }
 }
 //Leer 10 enteros, almacenarlos en un vector y determinar en qué posición del vector está el mayor número leído.
