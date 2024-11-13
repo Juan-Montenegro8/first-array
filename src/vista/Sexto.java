ad7ed486c5bc23c6c11f1d73fbef91939e7ba4c6
Juan <jj.monte2002@gmail.com>
2024-11-12 20:08:27 -0500

    sixth commit

--- src/vista/Sexto.java
+++ src/vista/Sexto.java
@@ -1,25 +1,20 @@
 package vista;
 
 import java.util.*;
-import java.util.Random;
 public class Sexto {
 
     public static void main(String[] args) {
         
-        Scanner t=new Scanner (System.in);
         int numeros[]= new int[10];
         Random aleatorio = new Random();
-        String max="";
+        int posicion=0;
         for (int i = 0; i < numeros.length; i++) {
             numeros[i]=aleatorio.nextInt(11);
-            int num;
-            num = numeros[0];
+            
             System.out.println("el numero es "+numeros[i]+" y la posicion "+i);
-            if(num<numeros[i]){
-                max=i+"";
-            }
+            
         }
-        System.out.println("la posicion del mayor es "+max);
+        System.out.println("la posicion del mayor es "+posicion);
     }
 }
 //Leer 10 enteros, almacenarlos en un vector y determinar en qué posición del vector está el mayor número leído.
