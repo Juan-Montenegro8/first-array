4dbbabeb1ed33e37390aca524e647c936efbead7
Juan <jj.monte2002@gmail.com>
2024-11-18 18:39:36 -0500

    tenth commit

--- src/vista/Novena.java
+++ src/vista/Novena.java
@@ -6,16 +6,17 @@ public class Novena {
 
     public static void main(String[] args) {
         
-        int num[]=new int[10];
-        Random ale=new Random();
-        int prm=0;
-        for (int i = 0; i < num.length; i++) {
-            num[i]=ale.nextInt(11);
-            System.out.println("numero "+num[i]);
-            prm=num[0]+num[1]+num[2]+num[3]+num[4]+num[5]
-                    +num[6]+num[7]+num[8]+num[9]/10;
+        int numero[]=new int[10];
+        Random aleatorio=new Random();
+        double promedio,contador=0,repeticiones=0;
+        for (int i = 0; i < numero.length; i++) {
+            numero[i]=aleatorio.nextInt(11);
+            System.out.println("Numero "+numero[i]);
+            contador+=numero[i];
+            repeticiones+=1;
         }
-        System.out.println("el promedio es "+prm);
+        promedio=contador/repeticiones;
+        System.out.println("el promedio es "+promedio);
     }
     
 }
